a6fb0652b866f4b899e4e11ba4fe69cd25a81fc1
Наталия Фарафонова <ferymfon1983@gmail.com>
2021-11-28 13:23:18 +0200

    Создала последний компонент

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import Container from './components/Conteiner/Conteiner';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 import evidense from './components/Profile/user.json';
 import data from './components/Statistics/data.json';
 import friends from './components/FriendList/friends.json';
+import transactions from './components/TransactionHistory/transactions.json';
 
 export default function App() {
   return (
@@ -17,8 +19,9 @@ export default function App() {
         stats={evidense.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />;
+      <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Container>
   );
 }
